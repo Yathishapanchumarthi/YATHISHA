c19f7a58d62213cac38ac44f5b7b15728703aa80
yathishap <yathishap@VMT13025.VALUEMOMENTUM.BIZ>
2023-01-19 02:37:33 -0800

    Second Commit

---
+++ yathisha/src/test/Assignment.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  // TODO Auto-generated method stub
+		int[] ar= {1,2,3};
+		int[] ar1= {4,5,6};
+		if(ar.equals(ar1))
+		    System.out.println("contains same elements");
+		else
+		    System.out.println("diff elements");
+		    }
+}
+
+
